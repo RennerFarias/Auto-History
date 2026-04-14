99651d13cd28b5a88e45585555ea3d87866a892e
Elian Barros <elian.b.siqueira@gmail.com>
2026-04-14 14:40:42 -0300

    Update storage.js
    Adicionado o link da pagina inicial a logo do projeto no menu

--- js/baseDoSistema/storage.js
+++ js/baseDoSistema/storage.js
@@ -4,9 +4,11 @@ const rootMenu = ReactDOM.createRoot(elementoMenu);
 rootMenu.render(
     <>
         <header className="cabecalho">
-            <h2 className="logo"><img src="img/logo.png" alt="Logo Auto History" width="100" height="100"/></h2>
-
+            <a href="index.html">
+                <h2 className="logo"><img src="img/logo.png" alt="Logo Auto History" width="100" height="100"/></h2>
+            </a>
             <nav className="menu">
+                
                 <a href="index.html#funcionalidades">Recursos</a>
                 <a href="index.html#comofunciona">Etapas</a>
                 <a href="oficina.html">Oficinas Parceiras</a>  
